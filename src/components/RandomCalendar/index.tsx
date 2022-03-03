f597aaaf229d9a4f9d1e75beea6c417c0c2181a5
Guilherme Henrique <devguiet@gmail.com>
2022-03-03 00:27:04 -0300

    Page Repo

--- src/components/RandomCalendar/index.tsx
+++ src/components/RandomCalendar/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import HeatMap from 'react-calendar-heatmap';
-import { subYears } from 'date-fns';
+import { subYears, isBefore, isSameDay, addDays } from 'date-fns';
 
 import { Container } from './styles';
+
+type HeatmapValue = { date: Date; count: number };
 
 const RandomCalendar: React.FC = () => {
   const startDate = subYears(new Date(), 1)
@@ -12,9 +14,22 @@ const RandomCalendar: React.FC = () => {
     <Container>
       <div className="wrapper">
         <HeatMap 
-          startDate={}
-          endDate={}
-          values={[]}
+          startDate={startDate}
+          endDate={endDate}
+          values={generateHeatmapValues(startDate, endDate)}
+          gutterSize={3.5}
+          classForValue={(item: HeatmapValue) => {
+            let clampedCount = 0;
+
+            if (item != null) {
+              clampedCount = Math.max(item.count, 0);
+              clampedCount = Math.min(item.count, 4);
+            }
+
+            return `scale-${clampedCount}`
+          }}
+          showWeekdayLabels
+
         />
       </div>
       
@@ -22,5 +37,21 @@ const RandomCalendar: React.FC = () => {
     </Container>
   );
 }
+
+const generateHeatmapValues = (startDate: Date, endDate: Date) => {
+  const values: HeatmapValue[] = [];
+
+  let currentDate = startDate;
+  while (isBefore(currentDate, endDate) || isSameDay(currentDate, endDate)) {
+    const count = Math.random() * 4;
+
+    values.push({ date: currentDate, count: Math.round(count) });
+
+    currentDate = addDays(currentDate, 1);
+
+  }
+
+  return values;
+};
 
 export default RandomCalendar;
--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { Container, BreadCrump, Stars, LinkButton, GithubIcon, ForkIcon, StarIcon, RepoIcon } from './styles';
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <BreadCrump>
+        <RepoIcon />
+        <Link className={'username'} to={'/devguiet'}>
+          devguiet
+        </Link>
+
+        <span>/</span>
+        
+        <Link className={'reponame'} to={'/devguiet/Clone-Github'}>
+          Clone-Github
+        </Link>
+      </BreadCrump>
+      <p>Contains all of my projects</p>
+      <Stars>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>       
+      </Stars>
+      <LinkButton href={'https://github.com/devguiet/Clone-Github'}>
+          <GithubIcon />
+          <span>View on Github</span>
+      </LinkButton>
+    </Container>
+  );
 }
 
 export default Repo;
